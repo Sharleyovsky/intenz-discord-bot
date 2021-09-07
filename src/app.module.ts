e735c5777068d08f6df34dee335271ab9cb9d28c
Jakub <gamernice223@gmail.com>
2021-09-07 22:38:26 +0200

    Create bot module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BotModule } from './bot/bot.module';
+import { BotGateway } from './bot.gateway';
 
 @Module({
   imports: [BotModule],
   controllers: [],
-  providers: [],
+  providers: [BotGateway],
 })
 export class AppModule {}
