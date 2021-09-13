f07705461f0dc39ee37f0820b12ed363cb0b59cd
Jakub <gamernice223@gmail.com>
2021-09-14 00:16:17 +0200

    Add function that returns Guild class

--- src/bot/bot.gateway.ts
+++ src/bot/bot.gateway.ts
@@ -1,10 +1,30 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Once, DiscordClientProvider } from 'discord-nestjs';
+import { Guild } from 'discord.js';
 
 @Injectable()
 export class BotGateway {
   private readonly logger = new Logger(BotGateway.name);
   constructor(private readonly discordProvider: DiscordClientProvider) {}
+
+  getGuild(name: string): Guild | null {
+    try {
+      const [guild] = this.discordProvider
+        .getClient()
+        .guilds.cache.map(
+          (guild) => guild.name.toLowerCase() === name.toLowerCase() && guild,
+        );
+
+      if (!guild) {
+        throw new Error('Guild not found!');
+      }
+
+      return guild;
+    } catch (error) {
+      this.logger.error(error);
+      return null;
+    }
+  }
 
   @Once({ event: 'ready' })
   async onReady(): Promise<void> {
