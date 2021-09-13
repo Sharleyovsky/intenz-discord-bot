3669769fdb7e84ff62d135b69ff0b3f9d083d089
Jakub <gamernice223@gmail.com>
2021-09-14 00:14:33 +0200

    Add discord bot config class

--- src/bot/bot-config.ts
+++ src/bot/bot-config.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { DiscordModuleOption, DiscordOptionsFactory } from 'discord-nestjs';
+import { Intents } from 'discord.js';
 
 @Injectable()
 export class BotConfig implements DiscordOptionsFactory {
+  private readonly Intents = new Intents();
+
   createDiscordOptions(): DiscordModuleOption {
+    this.Intents.add(
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_INTEGRATIONS,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.GUILD_INVITES,
+    );
     return {
-      intents: null,
+      intents: this.Intents,
       token: process.env.TOKEN,
       commandPrefix: '!',
     };
