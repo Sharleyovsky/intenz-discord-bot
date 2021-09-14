3d3a5b6a30adeebc57d93620a191840f1811ceb5
Jakub <gamernice223@gmail.com>
2021-09-14 02:18:35 +0200

    Add Cron support to the module

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { DiscordModule } from 'discord-nestjs';
 import { BotConfig } from './bot-config';
 import { BotGateway } from './bot.gateway';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [DiscordModule.forRootAsync({ useClass: BotConfig })],
+  imports: [
+    DiscordModule.forRootAsync({ useClass: BotConfig }),
+    ScheduleModule.forRoot(),
+  ],
   providers: [BotGateway],
 })
 export class BotModule {}
